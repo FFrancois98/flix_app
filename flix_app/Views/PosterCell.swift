b0fa73d3a0e78bd66e2b1bac3b4ade51314e2f0e
user144860 <user144860@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-09-11 19:03:54 -0400

    Added the collection view

---
+++ flix_app/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_app
+//
+//  Created by user144860 on 9/11/18.
+//  Copyright © 2018 Fleurevca Francois. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
